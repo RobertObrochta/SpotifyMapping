b7e437e4dce2fe30ac4c971009ce26932ec7b82c
Robert Obrochta <obrochtarobert13@gmail.com>
2020-12-01 21:48:59 -0600

    Update graphing.py
    Here is my final code for the graphing portion of this project

--- graphing.py
+++ graphing.py
@@ -1,23 +1,30 @@
+
 import main as sp
 from main import client_id, client_secret
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# keep reworking, fit labels into graph, and space them out (if I can)
-# look at possible options for drawing the graph 
-G = nx.star_graph(1)
+
+names = sp.SpotifyAPI(client_id, client_secret).artist_track_name()
+G = nx.Graph()
 
 options = {
     'node_color' : '#26E07C',
-    'edge_color' : '#26E07C',
-    'width' : 13,
+    'edge_color' : '#2F566C',
+    'width' : 3,
     'with_labels' : True,
 }
 
-names = sp.SpotifyAPI(client_id, client_secret).artist_track_name()
-
 # adds nodes from the list of names, returned in artist_track_name() function
 G.add_nodes_from(names)
+
+# this ensures that the range of r is within the bounds of the indices in 'names'
+# adds the connections between the nodes
+for i in range(len(names)):
+    r = i + 1
+    if r < 50:
+        G.add_edges_from([(names[i], names[r])])
+
 pos = nx.spring_layout(G)
 
 nx.draw(G, pos, **options)
